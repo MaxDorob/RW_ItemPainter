c89c6d15b3b82a7671cc1791c0d092a31fb39ae3
Maxim <MaximSw@web.de>
2024-05-24 14:55:55 +0200

    tool step

--- Source/RWPaintingTool/RWPaintingTool/Window_ThingColoring.cs
+++ Source/RWPaintingTool/RWPaintingTool/Window_ThingColoring.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using LudeonTK;
 using RimWorld;
 using TeleCore.AssetLoader;
@@ -20,16 +21,8 @@ public class Window_ThingColoring : Window
     
     private int colorIndex = 0;
 
-    private Material colorPickerMat;
-    private Material hueMat;
-    private Material satMat;
-    private Material valMat;
-    
-    private Material redMat;
-    private Material greenMat;
-    private Material blueMat;
-    
-    private Material alphaMat;
+
+    private ColorPicker _colorPicker;
 
     #region Tweaky
 
@@ -51,26 +44,7 @@ public class Window_ThingColoring : Window
         style.border = new RectOffset(2, 2, 2, 2);
         
         _thing = thing;
-        colorPickerMat = new Material(AssetBundleDB.LoadShader("ColorPickerGradient"));
-        hueMat = new Material(AssetBundleDB.LoadShader("ColorPickerHue"));
-        satMat = new Material(AssetBundleDB.LoadShader("ColorPickerHue"));
-        valMat = new Material(AssetBundleDB.LoadShader("ColorPickerHue"));
-        
-        redMat = new Material(AssetBundleDB.LoadShader("ColorPickerRGB"));
-        greenMat = new Material(AssetBundleDB.LoadShader("ColorPickerRGB"));
-        blueMat = new Material(AssetBundleDB.LoadShader("ColorPickerRGB"));
-        
-        hueMat.SetInt("_Invert", 1);
-        satMat.SetInt("_Invert", 1);
-        valMat.SetInt("_Invert", 1);
-        satMat.SetInt("_Mode", 1);
-        valMat.SetInt("_Mode", 2);
-        
-        greenMat.SetInt("_Result", 1);
-        blueMat.SetInt("_Result", 2);
-        
-        //hueMat.SetInt("_Invert", 1);
-        alphaMat = new Material(AssetBundleDB.LoadShader("ColorPickerAlpha"));
+        _colorPicker = new ColorPicker();
         
         _colorsSet = new SixColorSet
         {
@@ -107,149 +81,27 @@ public class Window_ThingColoring : Window
         Widgets.DrawMenuSection(rightPart);
         DrawColorTool(rightPart.ContractedBy(5).TopPartPixels(300).LeftPartPixels(300));
     }
-
-    private int[] hsvVals = new int[3] ;
-    private int[] colorVals = new int[3];
-    private string[] buffers = new string[3] ;
-    private string[] buffers2 = new string[3] ;
     
     private void DrawColorTool(Rect rect)
     {
-        //Widgets.DrawHighlight(rect);
-        var color = _colorsSet[colorIndex];
-        
-        colorVals = new[] {(int) (color.r * 255), (int) (color.g * 255), (int) (color.b * 255)};
-        
-       
-        var parts = (rect.height / 8) /2;
-        var colorSize = parts * 8 + (5 + 10 + (5*7));
-        var leftSide = rect.LeftPartPixels(colorSize);
-        var rightSide = new Rect(leftSide.xMax + 5, leftSide.y, rect.width / 2, rect.height);
-        var colorPicker = leftSide.TopPartPixels(colorSize);
+        var colorPickRect = rect.RightPart(0.85f);
+        var colorSelectArea = rect.LeftPart(0.15f);
 
-        //RectAggregator rectA = new RectAggregator(new Rect(rightSide.position, new Vector2(rightSide.width, 0)), this.GetHashCode());
-        var div = new RectDivider(rightSide, this.GetHashCode(), new Vector2(5, 5));
-        var HSV = div.NewRow(parts);
-        var HSV_H = div.NewRow(parts);
-        var HSV_S = div.NewRow(parts);
-        var HSV_V = div.NewRow(parts, marginOverride:10);
-        var textSize = Text.CalcSize("1000").x;
-        
-        var HSV_H_Label = HSV_H.NewCol(textSize/2);
-        var HSV_S_Label = HSV_S.NewCol(textSize/2);
-        var HSV_V_Label = HSV_V.NewCol(textSize/2);
-        
-        var HSV_H_Input = HSV_H.NewCol(textSize, HorizontalJustification.Right, 5);
-        var HSV_S_Input = HSV_S.NewCol(textSize, HorizontalJustification.Right, 5);
-        var HSV_V_Input = HSV_V.NewCol(textSize, HorizontalJustification.Right, 5);
-        
-        var RGB = div.NewRow(parts);
-        var RGB_R = div.NewRow(parts);
-        var RGB_G = div.NewRow(parts);
-        var RGB_B = div.NewRow(parts);
-        
-        var RGB_R_Label = RGB_R.NewCol(textSize/2);
-        var RGB_G_Label = RGB_G.NewCol(textSize/2);
-        var RGB_B_Label = RGB_B.NewCol(textSize/2);
-        
-        var RGB_R_Input = RGB_R.NewCol(textSize, HorizontalJustification.Right, 5);
-        var RGB_G_Input = RGB_G.NewCol(textSize, HorizontalJustification.Right, 5);
-        var RGB_B_Input = RGB_B.NewCol(textSize, HorizontalJustification.Right, 5);
-        
-        var huePicker = rect.LeftPart(0.15f).RightPart(0.5f).TopPart(0.8f);
-        var alphaPicker = rect.BottomPart(0.15f).TopPart(0.5f).RightPart(0.80f);
-        
-        Color.RGBToHSV(color, out var h, out var s, out var v);
-        hsvVals[0] = (int) (h * 360);
-        hsvVals[1] = (int) (s * 100);
-        hsvVals[2] = (int) (v * 100);
-        var colorSat = Color.HSVToRGB(h, 1, 1, true);
+        var div = (colorSelectArea.height - 20) / 6;
 
-        Vector2 uvPos = new Vector2(s, 1-v);
-        
-        colorPickerMat.SetFloat("_Hue", h);
-        redMat.SetColor("_Color", color);
-        greenMat.SetColor("_Color", color);
-        blueMat.SetColor("_Color", color);
-        
-        satMat.SetColor("_Color", colorSat);
-        valMat.SetColor("_Color", colorSat);
-        
-        //Color Picker
-        Graphics.DrawTexture(colorPicker, BaseContent.BadTex, 0, 0, 0, 0, colorPickerMat);
-        
-        var posRect = new Rect(colorPicker.position + (uvPos * colorPicker.size), new Vector2(7, 7));
-        Widgets.DrawBoxSolidWithOutline(posRect, color, Color.black);
-        
-        //RGB Group
-        //Widgets.Label(RGB, "RGB");
-        Widgets.Label(RGB, "RGB");
-        
-        Widgets.Label(RGB_R_Label, "R:");
-        Widgets.Label(RGB_G_Label, "G:");
-        Widgets.Label(RGB_B_Label, "B:");
-        
-        Graphics.DrawTexture(RGB_R, BaseContent.BadTex, 0, 0, 0, 0, redMat);
-        Graphics.DrawTexture(RGB_G, BaseContent.BadTex, 0, 0, 0, 0, greenMat);
-        Graphics.DrawTexture(RGB_B, BaseContent.BadTex, 0, 0, 0, 0, blueMat);
-        
-        GUI.TextField(RGB_R_Input, colorVals[0].ToString(), style); //Widgets.TextFieldNumeric(HSV_H_Input, ref testVals[0], ref buffers[0], 0, 360);
-        GUI.TextField(RGB_G_Input, colorVals[1].ToString(), style);   //Widgets.TextFieldNumeric(HSV_S_Input, ref testVals[1], ref buffers[1], 0, 100);
-        GUI.TextField(RGB_B_Input, colorVals[2].ToString(), style);         //Widgets.TextFieldNumeric(HSV_V_Input, ref testVals[2], ref buffers[2], 0, 100);
-        
-        //HSV Group
-        Widgets.Label(HSV, "HSV");
-        var size = Text.CalcSize("HSV");
-        //Widgets.DrawLineHorizontal(HSV.Rect.x + size.x, HSV.Rect.y + HSV.Rect.height/2, HSV.Rect.width - size.x);
-        
-        Widgets.Label(HSV_H_Label, "H:");
-        Widgets.Label(HSV_S_Label, "S:");
-        Widgets.Label(HSV_V_Label, "V:");
-        
-        Graphics.DrawTexture(HSV_H, BaseContent.BadTex, 0, 0, 0, 0, hueMat);
-        Graphics.DrawTexture(HSV_S, BaseContent.BadTex, 0, 0, 0, 0, satMat);
-        Graphics.DrawTexture(HSV_V, BaseContent.BadTex, 0, 0, 0, 0, valMat);
-
-        GUI.TextField(HSV_H_Input, hsvVals[0].ToString(), style); //Widgets.TextFieldNumeric(HSV_H_Input, ref testVals[0], ref buffers[0], 0, 360);
-        GUI.TextField(HSV_S_Input, hsvVals[1].ToString(), style);   //Widgets.TextFieldNumeric(HSV_S_Input, ref testVals[1], ref buffers[1], 0, 100);
-        GUI.TextField(HSV_V_Input, hsvVals[2].ToString(), style);         //Widgets.TextFieldNumeric(HSV_V_Input, ref testVals[2], ref buffers[2], 0, 100);
-        
-        
-        /*Color.RGBToHSV(color, out var h, out var s, out var v);
-        var colorSat = Color.HSVToRGB(h, 1, 1, true);
-
-        Vector2 uvPos = new Vector2(s, 1-v);
-        
-        colorPickerMat.SetFloat("_Hue", h);
-        alphaMat.SetColor("_Color", color);
-        alphaMat.SetVector("_Scale", new Vector4(alphaPicker.width, alphaPicker.height, 0, 0).normalized);
-        //colorPickerMat.SetFloat("_Alpha", );
-        
-        //Color Picker
-        Graphics.DrawTexture(colorPicker, BaseContent.BadTex, 0, 0, 0, 0, colorPickerMat);
-        Graphics.DrawTexture(huePicker, BaseContent.BadTex, 0, 0, 0, 0, hueMat);
-        Graphics.DrawTexture(alphaPicker, BaseContent.BadTex, 0, 0, 0, 0, alphaMat);
-        
-        var posRect = new Rect(colorPicker.position + (uvPos * colorPicker.size), new Vector2(7, 7));
-        Widgets.DrawBoxSolidWithOutline(posRect, color, Color.black);
-        
-        var hueRect = new Rect(huePicker.x - 2,  huePicker.y + (huePicker.height * (1-h)) + 4, huePicker.width + 4, 8);
-        Widgets.DrawBoxSolidWithOutline(hueRect, colorSat, Color.black);
-
-        var alphaLine = alphaPicker.x + alphaPicker.width * color.a;
-        Widgets.DrawLineVertical(alphaLine, alphaPicker.y -2, alphaPicker.height+4);*/
-    }
-
-    private void RGBSlider(Color color, int mode)
-    {
-        switch (mode)
+        var y = colorSelectArea.y + 5;
+        for (int i = 0; i < 6; i++)
         {
-            case 0:
+            var colorRect = new Rect(colorSelectArea.x + 5, y, colorSelectArea.width-10, div);
+            var color = _colorsSet[i];
+            Widgets.DrawBoxSolid(colorRect, color);
+            if (Widgets.ButtonInvisible(colorRect))
             {
-
+                _colorPicker.SetColor(color);
             }
-                break;
         }
+        
+        _colorPicker.Draw(colorPickRect);
     }
 
     private void DrawItemPreview(Rect rect)
@@ -273,33 +125,26 @@ public class Window_ThingColoring : Window
         DrawThing(bottomRect, _thing, Rot4.South, _colorsSet, 0);
     }
 
-    private RenderTexture WorkingTex;
-    private ComputeShader DrawShader;
-    
-    //Note: Some test stuff for drawing on a canvas texture
-    public void DrawingScene(Rect rect)
+    private void DrawMaskSelection(Rect rect)
     {
-        ComputeShader compute = DrawShader;
-        RenderTexture texture = WorkingTex; //A render texture that we feed into the compute shader
-
-        var canvasRect = rect;
+        var allMasks = GetTextures();
         
-        Widgets.BeginGroup(canvasRect);
-        var canvasArea = canvasRect.AtZero();
-        Widgets.DrawTextureFitted(canvasArea, texture, 1);
+        
+    }
 
-        var mouse = Event.current.mousePosition;
-        var mouseNormalized = mouse - canvasRect.position;
-
-        var mouseDown = Event.current.isMouse && Event.current.button == 0; //Left mouse down
-        compute.SetBool("_IsDrawing", mouseDown);
-        if (mouseDown)
+    private List<Texture2D> GetTextures()
+    {
+        if(this._thing is Apparel apparel)
         {
-            compute.SetVector("_MousePos", mouseNormalized);
-            //This tells the shader where to draw on the rendertexture
+            return MaskManager.GetMasksMulti(apparel.def, BodyTypeDefOf.Male, Rot4.North);
         }
         
-        Widgets.EndGroup();
+        if (_thing is Building b)
+        {
+            return MaskManager.GetMasksMulti(b.def, Rot4.North);
+        }
+        
+        return MaskManager.GetMasksSingle(_thing.def);
     }
     
     private void DrawThing(Rect rect, Thing thing, Rot4 rot, SixColorSet colors, int maskId)
@@ -357,10 +202,5 @@ public class Window_ThingColoring : Window
         material.SetColor("_ColorFive", colors.ColorFive);
         material.SetColor("_ColorSix", colors.ColorSix);
         material.SetTexture("_MaskTex", MaskManager.GetMask(mask));
-    }
-    
-    private void DrawColorPicker(Rect rect)
-    {
-        
     }
 }
