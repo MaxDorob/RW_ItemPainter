3c6db87ce68a47584b6b073db64c4876c0a05fc2
JodemLee <107512540+JodemLee@users.noreply.github.com>
2025-06-01 02:18:03 -0400

    Implemented Mask Swapping

--- Source/RWPaintingTool/RWPaintingTool/PaintingTool_Right.cs
+++ Source/RWPaintingTool/RWPaintingTool/PaintingTool_Right.cs
@@ -11,9 +11,14 @@ namespace RWPaintingTool
 {
     public partial class PaintingTool
     {
+
         private void DrawRightRect(Rect inRect)
         {
+            if (_thing == null || _thing.def == null) return;
+
             inRect.SplitHorizontally(ButSize.y, out var applyButtonRect, out inRect);
+
+            // Draw reset buttons
             if (Widgets.ButtonText(applyButtonRect.LeftPart(2f / 3f), "Reset".Translate()))
             {
                 ColorTracker.TempColorSet = ColorTracker.ColorSet;
@@ -24,13 +29,98 @@ namespace RWPaintingTool
                 _colorPicker.SetColors(ColorTracker.ColorSet[_curColorIndex]);
             }
 
-            inRect.SplitHorizontally(ButSize.y, out var masksRect, out var colorToolRect);
-
-
+            inRect.SplitHorizontally(ButSize.y, out var masksRect, out inRect);
             DrawMaskEditableColors(masksRect, _thing as Apparel);
+
+            inRect.SplitHorizontally(ColorPicker.DefaultSize.y, out var colorToolRect, out var maskSelectionRect);
             DrawColorTool(colorToolRect);
 
+            var paintableExt = _thing.def.GetModExtension<PaintableExtension>();
+            if (paintableExt?.maskCount > 0)
+            {
+                DrawMaskSelection(maskSelectionRect, paintableExt);
+            }
         }
+
+        private void DrawMaskSelection(Rect rect, PaintableExtension ext)
+        {
+            var maskTracker = ColorTrackerDB.GetMaskTracker(_thing);
+            if (maskTracker == null || !(_thing is Apparel apparel)) return;
+            if (!ApparelGraphicRecordGetter.TryGetGraphicApparel(apparel, pawn?.story?.bodyType, out var baseRecord)) return;
+
+            float rowHeight = 60f;
+            float spacing = 5f;
+            float buttonWidth = 40f;
+            float previewSize = rowHeight;
+
+            Rect rowRect = new Rect(rect.x, rect.y, rect.width, rowHeight);
+            int originalMaskID = maskTracker.CurMaskID;
+
+            for (int i = 0; i < ext.maskCount; i++)
+            {
+                if (rowRect.yMax > rect.yMax) break;
+
+                var buttonRect = new Rect(rowRect.x, rowRect.y, buttonWidth, rowHeight);
+                var previewRect = new Rect(buttonRect.xMax + spacing, rowRect.y, previewSize, previewSize);
+
+                bool isSelected = i == originalMaskID;
+                if (isSelected)
+                    Widgets.DrawHighlightSelected(rowRect);
+                else if (Mouse.IsOver(rowRect))
+                    Widgets.DrawHighlight(rowRect);
+
+                if (Widgets.ButtonText(buttonRect, i.ToString()))
+                {
+                    maskTracker.SetMaskID(i);
+                    UpdateCurrentMaskWithTracker();
+                    originalMaskID = i;
+                }
+
+                int prevMaskID = maskTracker.CurMaskID;
+
+                maskTracker.SetMaskID(i);
+                if (ApparelGraphicRecordGetter.TryGetGraphicApparel(apparel, pawn?.story?.bodyType, out var graphicRecord))
+                {
+                    maskTracker.SetMaskOn(graphicRecord.graphic);
+                    GraphicsPatches.CurThing = _thing;
+                    var mat = graphicRecord.graphic.MatAt(rotation);
+                    GenUI.DrawTextureWithMaterial(previewRect.Rounded(), mat.mainTexture, mat);
+                    GraphicsPatches.CurThing = null;
+                }
+
+                maskTracker.SetMaskID(prevMaskID);
+                UpdateCurrentMaskWithTracker();
+
+                rowRect.y += rowHeight + spacing;
+            }
+        }
+
+        private void UpdateCurrentMaskWithTracker()
+        {
+            if (_thing == null) return;
+
+            var maskTracker = ColorTrackerDB.GetMaskTracker(_thing);
+            if (maskTracker == null) return;
+
+            if (_thing is Apparel apparel)
+            {
+                if (ApparelGraphicRecordGetter.TryGetGraphicApparel(apparel, apparel.Wearer?.story?.bodyType, out var graphicRecord))
+                {
+                    maskTracker.SetMaskOn(graphicRecord.graphic);
+                    apparel.Notify_ColorChanged();
+                }
+            }
+            else
+            {
+                Graphic graphic = _thing.Graphic;
+                if (graphic != null)
+                {
+                    maskTracker.SetMaskOn(graphic);
+                    _thing.DirtyMapMesh(_thing.Map);
+                }
+            }
+        }
+
         private void DrawColorTool(Rect rect)
         {
 
